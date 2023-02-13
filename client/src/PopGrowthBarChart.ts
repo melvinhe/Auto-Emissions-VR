3c7e7339cf701329f7e24766ebc1410ffccc3906
Melvin He <melvin_he@brown.edu>
2023-02-12 23:05:42 -0500

    Update PopGrowthBarChart.ts

--- client/src/PopGrowthBarChart.ts
+++ client/src/PopGrowthBarChart.ts
@@ -1,17 +1,6 @@
 import { addBarGraph } from "./BarGraph";
 
-/*
-Car emissions data:
-https://www.bts.gov/content/estimated-national-average-vehicle-emissions-rates-vehicle-vehicle-type-using-gasoline-and
 
-
-
-
-
-
-
-
-*/
 const data2021 = [
   {
     automobile: "Light-duty vehicles",
@@ -51,39 +40,59 @@ const data2021 = [
   {
     region: "Motorcycles",
     carbon_monoxide: 4.418,
-    growth: 0.623,
+    nitrogen_oxides: 0.623,
   },
 ];
 
 const data2020 = [
   {
-    region: "Northeast",
-    population: 57525633,
-    growth: 17.4,
+    automobile: "Light-duty vehicles",
+    carbon_monoxide: 3.956,
+    nitrogen_oxides: 0.173,
   },
   {
-    region: "Midwest",
-    population: 68935174,
-    growth: 20.8,
+    automobile: "Light-duty trucks",
+    carbon_monoxide: 5.046,
+    nitrogen_oxides: 0.334,
   },
   {
-    region: "West",
-    population: 7863266,
-    growth: 23.7,
+    automobile: "Heavy-duty vehicles",
+    carbon_monoxide: 14.104,
+    nitrogen_oxides: 0.772,
   },
   {
-    region: "South",
-    population: 126409007,
-    growth: 38.1,
+    automobile: "Motorcycles",
+    carbon_monoxide: 13.2,
+    nitrogen_oxides: 0.64,
+  },
+  {
+    automobile: "Light-duty vehicles",
+    carbon_monoxide: 3.784,
+    nitrogen_oxides: .133,
+  },
+    {
+    automobile: "Light-duty trucks",
+    carbon_monoxide: 2.211,
+    nitrogen_oxides: 1.543,
+  },
+  {
+    automobile: "Heavy-duty vehicles",
+    carbon_monoxide: 1.923,
+    nitrogen_oxides: 3.822,
+  },
+  {
+    region: "Motorcycles",
+    carbon_monoxide: 4.418,
+    nitrogen_oxides: 0.623,
   },
 ];
 
 const scene = document.querySelector("a-scene")!;
 
-const maxPopulation2021 = Math.max(...data2021.map((d) => d.population));
-const maxPopulation2020 = Math.max(...data2020.map((d) => d.population));
-// const maxGrowth2021 = Math.max(...data2021.map((d) => d.growth));
-// const maxGrowth2020 = Math.max(...data2020.map((d) => d.growth));
+const maxPopulation2021 = Math.max(...data2021.map((d) => d.carbon_monoxide));
+const maxPopulation2020 = Math.max(...data2020.map((d) => d.carbon_monoxide));
+// const maxGrowth2021 = Math.max(...data2021.map((d) => d.nitrogen_oxides));
+// const maxGrowth2020 = Math.max(...data2020.map((d) => d.nitrogen_oxides));
 
 let i = 0;
 i = 0;
